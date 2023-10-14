79e22a86e27aac2993afc0899abb70300409167d
Vasile Jureschi <vasile.jureschi@gmail.com>
2023-10-14 15:04:46 +0300

    Add MockitoExtension to FileProcessorTest and remove unused imports and annotations.

--- src/test/java/org/jenkinsci/plugins/pitmutation/FileProcessorTest.java
+++ src/test/java/org/jenkinsci/plugins/pitmutation/FileProcessorTest.java
@@ -3,13 +3,16 @@ package org.jenkinsci.plugins.pitmutation;
 import hudson.FilePath;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class FileProcessorTest {
 
     @Mock
@@ -22,7 +25,6 @@ class FileProcessorTest {
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
         fileProcessor = new FileProcessor();
     }
 
